074eb1d45a11f87d8652b8964bcd7a01ca028445
keshviiipatel <keshviiipatel@gmail.com>
2022-09-19 22:19:24 +0530

    name indicate

--- path_core_module.js
+++ path_core_module.js
@@ -1,29 +1,36 @@
 // "PATH_CORE_MODULE":
 
+// 1)basename:
 var path = require('path')
 console.log(path.basename('/Users/Refsnes/demo_path.js'))
 // Ans="demo_path.js"
 
+// 2)dirname:
 var path = require('path')
 console.log(path.dirname('/Users/Refsnes/demo_path.js'))
 // Ans="/Users/Refsnes"
 
+// 3)normalize:
 var path = require('path')
 console.log(path.normalize('Users/Refsnes/../jackson'))
 // Ans="Users\jackson"
 
+// 4)join:
 var path = require('path')
 console.log(path.join('Users','Refsnes'))
 // Ans="Users\Refsnes"
 
+// 5)resolve:
 var path = require('path')
 console.log(path.resolve('Users','abc'))
 // Ans="C:\Users\ASUS\Documents\SEM_3\WEB_TECNOLOGY\NODE_PROJECTS\Path_Core_Module\Users\abc"
 
+// 6)relative:
 var path = require('path')
 console.log(path.relative('Users','nodeproject'))
 // Ans="..\nodeproject"
 
+// 7)extname:
 var path = require('path')
 console.log(path.extname('Node/Users.Js'))
 // Ans=".Js"
